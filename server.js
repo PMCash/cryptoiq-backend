4f2312f63e52c5adb1cefd8f43a871aa6f6d8f55
PMCash <e051803.nwakusor@dlc.ui.edu.ng>
2026-01-13 04:45:42 +0100

    Update X-country small letter

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.use(
       "https://cryptoiq-frontend-jsl1.onrender.com",
     ],
     methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization" , "X-Country"],
+    allowedHeaders: ["Content-Type", "Authorization" , "X-country"],
     credentials: true,
   })
 );
