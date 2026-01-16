3b3c57aab19a9ea6f8f0f5de8d7b5c164c924342
PMCash <e051803.nwakusor@dlc.ui.edu.ng>
2026-01-16 13:54:47 +0100

    supabase workings

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ if (!process.env.SUPABASE_URL || !process.env.SUPABASE_SERVICE_ROLE_KEY) {
   console.error("❌ Missing Supabase environment variables!");
   process.exit(1);
 }
-const { createClient } = require("@supabase/supabase-js");
+
 // Admin client – used for DB writes & webhooks
 const supabaseAdmin = createClient(
   process.env.SUPABASE_URL,
